fd650a041ed4e72ac0cf7a7e1d6ac2feb6f6a180
leonardordss <lrodrigues.md@gmail.com>
2025-02-13 20:01:05 -0300

    Logica sensores

--- Aula03/Projeto2/main.ino
+++ Aula03/Projeto2/main.ino
@@ -1,2 +1,39 @@
-#define BUTTON_PIN 0
-#define LED_PIN 1
+#define ON_BUTTON_PIN 9
+#define BAR_SENSOR_PIN 1
+#define WINDOW1_SENSOR_PIN 2
+#define WINDOW2_SENSOR_PIN 3
+#define ROOM_SENSOR_PIN 4
+#define KITCHEN_SENSOR_PIN 5
+#define GARAGE_SENSOR_PIN 6
+#define HALL_SENSOR_PIN 7
+#define ALARM_PIN 8
+
+bool onButton = 0, bar_sensor = 0, window1_sensor = 0;
+bool window2_sensor = 0, room_sensor = 0, kitchen_sensor = 0;
+bool garage_sensor = 0, hall_sensor = 0, alarm = 0;
+
+void setup()
+{
+    pinMode(ON_BUTTON_PIN,INPUT_PULLUP);
+    pinMode(BAR_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(WINDOW1_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(WINDOW2_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(ROOM_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(KITCHEN_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(GARAGE_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(HALL_SENSOR_PIN, INPUT_PULLUP);
+    pinMode(ALARM_PIN,OUTPUT);
+}
+
+void loop(){
+
+    onButton = digitalRead(ON_BUTTON_PIN);
+    bar_sensor = digitalRead(BAR_SENSOR_PIN);
+    window1_sensor = digitalRead(WINDOW1_SENSOR_PIN);
+    window2_sensor = digitalRead(WINDOW2_SENSOR_PIN);
+    room_sensor = digitalRead(ROOM_SENSOR_PIN);
+    kitchen_sensor = digitalRead(KITCHEN_SENSOR_PIN);
+    garage_sensor = digitalRead(GARAGE_SENSOR_PIN);
+    hall_sensor = digitalRead(HALL_SENSOR_PIN);
+    digitalWrite(ALARM_PIN, alarm);
+}
