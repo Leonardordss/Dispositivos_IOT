9fabb96d56c1207f8139dee8bdf7111e835118ab
leonardordss <lrodrigues.md@gmail.com>
2025-02-27 20:18:10 -0300

    Páginas Environment e Notfound

--- DOMOTICA/domotica/src/views/Devices.vue
+++ DOMOTICA/domotica/src/views/Devices.vue
@@ -1,22 +1,38 @@
 <script setup lang="ts">
 
-import {Places} from '@/models/devices';
+import {Environment} from '@/models/devices';
 import {reactive, ref} from 'vue';
 
-const sala: Places = {
-    nome: 'Sala de Estar',
-    devices: []
-}
+const environments = reactive(Array<Environment>);
 
-const contador = ref(0);
-const somar = ()=> contador.value += 1;
+
+const sala: Environment = reactive({
+    name: 'Sala de Estar',
+    devices: [
+        {
+            name:'Ar condicionado',
+            state: false,
+            color: '#3266a8'
+        },
+        {
+            name:'Smart TV',
+            state: true,
+            color: '#a5c949'
+        },
+        {
+            name:'Iluminação',
+            state: false,
+            color: '#4d0d75'
+        },
+    ]
+})
 
 </script>
 
 <template>
-    <h1>Contagem: {{ contador }}</h1>
-    <button @click="somar">Somar</button>
-
+    <h1>Devices page!!!!</h1>
+    {{ sala }}
+    
 </template>
 
 <style scoped lang="scss">
