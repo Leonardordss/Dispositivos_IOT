d6270ae7461d47ca8e78de413beef2c9792dca3b
leonardordss <lrodrigues.md@gmail.com>
2025-02-27 22:18:48 -0300

    Criando botões na tela

--- DOMOTICA/domotica/src/models/devices.ts
+++ DOMOTICA/domotica/src/models/devices.ts
@@ -3,6 +3,8 @@ export class Device {
     name: string = "";
     state: boolean = false;
     color: string = "#827d85";
+    icon: string = "devices";
+   
 }
 
 export class Environment {
--- DOMOTICA/domotica/src/views/Devices.vue
+++ DOMOTICA/domotica/src/views/Devices.vue
@@ -8,15 +8,18 @@ const environments: Array<Environment> = reactive([]);
 const ar: Device = reactive(new Device());
 ar.name = 'Ar condicionado';
 ar.color = '#3266a8';
+ar.icon = 'mode_fan';
 
 const tv: Device = reactive(new Device());
 tv.name ='Smart TV';
 tv.color = '#a5c949';
 tv.state = true;
+tv.icon = 'tv';
 
 const iluminacao: Device = reactive(new Device());
 iluminacao.name = 'Iluminação';
 iluminacao.color = '#4d0d75';
+iluminacao.icon = 'light';
 
 const sala: Environment = reactive(new Environment());
 sala.name = 'Sala';
@@ -29,9 +32,17 @@ environments.push(sala);
 <template>
     <main class="flex flex-column text-center justify-content align-items-center">
         <h1>Devices page!!!!</h1>
-        <section class="environments flex flex-column">
-
-
+        <section class="environments flex flex-column border-round-sm">
+            <div class="device" v-for="(environment, env_id) in environments" :key="env_id">
+                <h3>{{ environment.name }}</h3>
+                <div v-for="(device, dev_id) in environment.devices" :key="dev_id">
+                    <section>
+                        <h5>{{ device.name }}</h5>
+                        <button>ON</button>
+                        <button>OFF</button>
+                    </section>
+                </div>
+            </div>
         </section>
     </main>
     
@@ -43,9 +54,10 @@ main{
     width: 100vw;
     min-height: 100vh;
     .environments{
-        width: 98vw;
+        width: 90vw;
         min-height: 95vh;
         background-color: rgb(206, 160, 160);
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     }
 }
 
